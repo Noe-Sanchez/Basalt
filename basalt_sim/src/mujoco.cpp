2f8dfc5ba5b7c4b9e6297b0daed1a94d02e8138b
Noe-Sanchez <danfoot4567@gmail.com>
2026-08-20 17:52:47 -0600

    Homogeneous generalized MuJoCo multidrone support. Needs cleanup and a way to load heterogenous swarms

--- basalt_sim/src/mujoco.cpp
+++ basalt_sim/src/mujoco.cpp
@@ -130,7 +130,6 @@ class Multirotor {
 	  break;
 	}
       }
-      //std::cout << "Drone: " << drone_name << ", Body index: " << body_idx << ", Joint index: " << joint_idx << std::endl;
       std::cout.flush();
 
     }
@@ -138,32 +137,19 @@ class Multirotor {
     // Adapted to handle only the variables of a single drone at a time
     void multicopter_motor_physics(){
       
-      // Clear rotor_count forces starting from the joint_idx position
-      //mju_zero(d->qfrc_applied + joint_idx, rotor_count);
-
       // Check for base_link
       double dt = m->opt.timestep;
-      //int base_id = mj_name2id(m, mjOBJ_BODY, "base_link");
-      //if (base_id < 0) return;
 
       Eigen::Vector3d wind_vel_world(0.0, 0.0, 0.0);
 
       for (int i = 0; i < rotor_count; i++) { 
         // Check for rotor
-        //std::string rotor_name = "rotor_" + std::to_string(i);
-        //std::string joint_name = rotor_name + "_joint";
-        //int rotor_id = mj_name2id(m, mjOBJ_BODY,  rotor_name.c_str());
-        //int joint_id = mj_name2id(m, mjOBJ_JOINT, joint_name.c_str());
 	int rotor_id = body_idx + i + 1;
 	int joint_id = joint_idx + i;
-
-        //std::cout << "Drone: " << drone_name << ", Rotor: " << i << ", Rotor ID: " << rotor_id << ", Joint ID: " << joint_id << std::endl;
 
         if (rotor_id < 0 || joint_id < 0) continue;
 
         int dof_idx = m->jnt_dofadr[joint_id];
-
-	//std::cout << "Drone: " << drone_name << ", Rotor: " << i << ", Rotor ID: " << rotor_id << ", Joint ID: " << joint_id << ", DOF index: " << dof_idx << std::endl;
 
         // First order filter from original plugin
         double refMotorInput = target_omega[i];
@@ -262,92 +248,6 @@ class Multirotor {
 // Pointer to drones array, to be used in the callback
 std::vector<Multirotor>* drones_ptr = nullptr;
 
-/*void multicopter_motor_callback(const mjModel* m, mjData* d) {
-  // Clear forces to avoid accum
-  mju_zero(d->qfrc_applied, m->nv);
-
-  // Check for base_link
-  double dt   = m->opt.timestep;
-  int base_id = mj_name2id(m, mjOBJ_BODY, "base_link");
-  if (base_id < 0) return;
-
-  Eigen::Vector3d wind_vel_world(0.0, 0.0, 0.0);
-
-  for (int i = 0; i < 4; i++) {
-    // Check for rotor
-    std::string rotor_name = "rotor_" + std::to_string(i);
-    std::string joint_name = rotor_name + "_joint";
-    int rotor_id = mj_name2id(m, mjOBJ_BODY, rotor_name.c_str());
-    int joint_id = mj_name2id(m, mjOBJ_JOINT, joint_name.c_str());
-    if (rotor_id < 0 || joint_id < 0) continue;
-
-    int dof_idx = m->jnt_dofadr[joint_id];
-
-    // First order filter from original plugin
-    double refMotorInput = target_omega[i];
-    if (refMotorInput < 0.0) refMotorInput = 0.0;
-    if (refMotorInput > kMaxRotVelocity) refMotorInput = kMaxRotVelocity;
-    double cur = current_omega[i];
-    double tau = (refMotorInput > cur) ? kTimeConstantUp : kTimeConstantDown;
-    double alpha = std::exp(-dt / tau);
-    double refMotorRotVel = alpha * cur + (1.0 - alpha) * refMotorInput;
-    current_omega[i] = refMotorRotVel;
-
-    // Velocity sign correct
-    double motorRotVel = d->qvel[dof_idx];
-    double realMotorVelocity = motorRotVel * kRotorVelocitySlowdownSim;
-    int realMotorVelocitySign = (realMotorVelocity > 0) - (realMotorVelocity < 0);
-
-    // 3. World Kinematics
-    mjtNum rotor_vel[6];
-    mj_objectVelocity(m, d, mjOBJ_BODY, rotor_id, rotor_vel, 0);
-    Eigen::Vector3d body_lin_vel_world(rotor_vel[3], rotor_vel[4], rotor_vel[5]);
-
-    mjtNum axis_local[3] = {0, 0, 1};
-    mjtNum axis_world_raw[3];
-    mju_mulMatVec3(axis_world_raw, d->xmat + 9 * rotor_id, axis_local);
-    Eigen::Vector3d axis_world(axis_world_raw[0], axis_world_raw[1], axis_world_raw[2]);
-    axis_world.normalize();
-
-    // Compute thrust
-    double thrust = kSpinDir[i] * realMotorVelocitySign * (realMotorVelocity * realMotorVelocity) * kMotorConstant;
-    Eigen::Vector3d thrust_force_world = thrust * axis_world;
-
-    // Get wind efffects
-    Eigen::Vector3d relative_wind_vel = body_lin_vel_world - wind_vel_world;
-    Eigen::Vector3d v_perp = relative_wind_vel - (relative_wind_vel.dot(axis_world) * axis_world);
-
-    Eigen::Vector3d air_drag = -std::abs(realMotorVelocity) * kRotorDragCoefficient * v_perp;
-    Eigen::Vector3d rolling_moment = -std::abs(realMotorVelocity) * kRollingMomentCoefficient * v_perp;
-
-    // Compute on-rotor force and torque
-    Eigen::Vector3d rotor_force = thrust_force_world + air_drag;
-    mjtNum force_rotor_global[3] = {rotor_force.x(), rotor_force.y(), rotor_force.z()};
-    mjtNum torque_rotor_global[3] = {0, 0, 0};
-    mjtNum point_rotor_global[3] = {d->xpos[3 * rotor_id], d->xpos[3 * rotor_id + 1], d->xpos[3 * rotor_id + 2]};
-
-    mj_applyFT(m, d, force_rotor_global, torque_rotor_global, point_rotor_global, rotor_id, d->qfrc_applied);
-
-    Eigen::Vector3d drag_torque_world = -kSpinDir[i] * thrust * kMomentConstant * axis_world;
-    Eigen::Vector3d parent_torque = drag_torque_world + rolling_moment;
-
-    mjtNum force_parent_global[3]  = {0, 0, 0};
-    mjtNum torque_parent_global[3] = {parent_torque.x(), parent_torque.y(), parent_torque.z()};
-    mjtNum point_parent_global[3]  = {d->xpos[3 * body_idx], d->xpos[3 * body_idx + 1], d->xpos[3 * body_idx + 2]};
-
-    //mj_applyFT(m, d, force_parent_global, torque_parent_global, point_parent_global, base_id, d->qfrc_applied);
-    mj_applyFT(m, d, force_parent_global, torque_parent_global, point_parent_global, body_idx, d->qfrc_applied);
-
-    // Joint velocity command
-    double desired_qvel = (kSpinDir[i] * refMotorRotVel) / kRotorVelocitySlowdownSim;
-    double current_qvel = d->qvel[dof_idx];
-    double rotor_inertia = 2.649858234714004e-05;
-
-    double required_torque = rotor_inertia * (desired_qvel - current_qvel) / dt;
-    d->qfrc_applied[dof_idx] += required_torque;
-  }
-}*/
-
 // UI callbacks (from MuJoCo sample code)
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods) {
   if (act == GLFW_PRESS && key == GLFW_KEY_BACKSPACE) {
@@ -406,41 +306,29 @@ void simulation_step_callback(const mjModel* m, mjData* d){
 class MujocoNode : public rclcpp::Node {
 public:
   MujocoNode() : Node("mujoco_node") {
-    // Model param
-    //this->declare_parameter<std::string>("mujoco_model", "mujoco/x500.xml");
-    //mujoco_model = this->get_parameter("mujoco_model").as_string();
-
-    /*char error[1000] = "Could not load XML model";
-    sim_model = mj_loadXML(mujoco_model.c_str(), nullptr, error, 1000);
-    if (!sim_model) {
-      RCLCPP_ERROR(this->get_logger(), "Failed to load Mujoco model: %s", error);
-      throw std::runtime_error("MuJoCo load failed");
-    } else {
-      RCLCPP_INFO(this->get_logger(), "Loaded Mujoco model: %s", mujoco_model.c_str());
-
-    }*/
-
-    // Hardcode model composition for now
-    // Replicate this python code in cpp
-    // world = mj.MjSpec.from_file("mujoco/world.xml")
-
-    // # Compose the world model with the other xml files
-    // drone1 = mj.MjSpec.from_file("mujoco/x500_standalone.xml")
-    // drone2 = mj.MjSpec.from_file("mujoco/x500_standalone.xml")
-    // 
-    // # Compose here
-    // world_site = world.worldbody.add_site(pos=[0, 0, 0], name="world_attachment_site")
-    // for i in range(2):
-    //   world.attach(child=drone1 if i == 0 else drone2, prefix=f"/x500_{i+1}/", site=world_site)
-    // 
-    // m = world.compile()
-    // d = mj.MjData(m)
     this->declare_parameter<int>("num_drones", 2);
     num_drones = this->get_parameter("num_drones").as_int();
 
     drones.reserve(num_drones);
 
     std::cout << "Creating dynamic publishers and subscribers for " << num_drones << " drones." << std::endl;
+    std::cout.flush();
+
+    // Load spec, not model
+    auto world     = mj_parseXML("mujoco/world.xml", nullptr, nullptr, 0);
+    
+    std::cout << "Loaded world" << std::endl;
+    std::cout.flush();
+    
+    auto worldbody = mjs_findBody(world, "world");
+    
+    std::cout << "Loaded worldbody" << std::endl;
+    std::cout.flush();
+    
+    std::cout << "Loaded world" << std::endl;
+    std::cout.flush();
+
+    std::cout << "Loaded xmls" << std::endl;
     std::cout.flush();
 
     for (int i = 0; i < num_drones; i++) {
@@ -457,51 +345,16 @@ public:
 
       std::cout << "Created publisher for drone: " << drone_name << std::endl;
       std::cout.flush();
+      
+      // Per drone type specs
+      auto x500_spec = mj_parseXML("mujoco/x500_standalone.xml", nullptr, nullptr, 0);
+
+      // Create drone spec 
+      mjs_attach(worldbody->element, x500_spec->element, drone_name.c_str(), "");
     
     }
-
-    std::cout << "Dynamic publishers and subscribers created." << std::endl;
-    std::cout.flush();
- 
+    
     drones_ptr = &drones;
-
-    // Load spec, not model
-    auto world  = mj_parseXML("mujoco/world.xml", nullptr, nullptr, 0);
-    auto drone1 = mj_parseXML("mujoco/x500_standalone.xml", nullptr, nullptr, 0);
-    auto drone2 = mj_parseXML("mujoco/x500_standalone.xml", nullptr, nullptr, 0);
-
-    std::cout << "MuJoCo XML models parsed." << std::endl;
-    std::cout.flush();
-
-    // Compose here (add site)
-    auto worldbody  = mjs_findBody(world, "world");
-    /*auto world_site = mjs_addSite(worldbody, nullptr);
-    world_site->pos[0]  = 0.0;
-    world_site->pos[1]  = 0.0;
-    world_site->pos[2]  = 0.0;
-    world_site->size[0] = 0.01;
-    world_site->size[1] = 0.01;
-    world_site->size[2] = 0.01;
-
-    std::cout << "World site added to MuJoCo model." << std::endl;
-    std::cout.flush();*/
-
-    //auto drone1_worldbody = mjs_findBody(drone1, "world");
-    //auto drone2_worldbody = mjs_findBody(drone2, "world");
-
-    //mjs_attach(world_site->element, drone1->element, "/x500_1/", nullptr);
-    //mjs_attach(world_site->element, drone2->element, "/x500_2/", nullptr);
-    
-    //mjs_attach(world_site->element, drone1_worldbody->element, "/x500_1/", "");
-    //mjs_attach(world_site->element, drone2_worldbody->element, "/x500_2/", "");
-    
-    //mjs_attach(worldbody->element, drone1_worldbody->element, "/x500_1/", "");
-    //mjs_attach(worldbody->element, drone2_worldbody->element, "/x500_2/", "");
-    
-    mjs_attach(worldbody->element, drone1->element, "/x500_1/", "");
-    mjs_attach(worldbody->element, drone2->element, "/x500_2/", "");
-
-    std::cout << "Drones attached to world site in MuJoCo model." << std::endl;
 
     sim_model = mj_compile(world, nullptr);
     sim_data  = mj_makeData(sim_model);
@@ -512,12 +365,6 @@ public:
       std::string drone_name = "/x500_" + std::to_string(i + 1) + std::string("/");
       drones.push_back(Multirotor(sim_model, sim_data, 4, i, drone_name));
 
-      /*// Change drone position to avoid collision
-      sim_data->xpos[3 * drones[i].body_idx]     = 0.0;
-      sim_data->xpos[3 * drones[i].body_idx + 1] = 1.0 * i; // Offset in y-axis
-      sim_data->xpos[3 * drones[i].body_idx + 2] = 0.5; // Height*/
-      
-      // Change drone position to avoid collision by modifying the free joint's state
       std::string free_joint_name = drone_name + "base_free_joint";
       int jnt_id = mj_name2id(sim_model, mjOBJ_JOINT, free_joint_name.c_str());
       
@@ -600,19 +447,6 @@ public:
 private:
 
   void motor_callback(const actuator_msgs::msg::Actuators::SharedPtr msg, int drone_id) {
-    /*int num_motors = 0;
-
-    if (!msg->velocity.empty()) {
-      num_motors = std::min(4, (int)msg->velocity.size());
-      for (int i = 0; i < num_motors; i++) {
-        target_omega[i] = msg->velocity[i];
-      }
-    } else if (!msg->normalized.empty()) {
-      num_motors = std::min(4, (int)msg->normalized.size());
-      for (int i = 0; i < num_motors; i++) {
-        target_omega[i] = msg->normalized[i] * kMaxRotVelocity;
-      }
-    }*/
 
     for (long unsigned int i = 0; i < (long unsigned int)drones[drone_id].rotor_count; i++) {
       if (i < msg->velocity.size()) {
